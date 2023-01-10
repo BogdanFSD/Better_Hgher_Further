bc5133328a0fa10031cba1fab37360bbf60ef5a3
Bogdandev <luitko007@gmail.com>
2023-01-10 12:59:41 +0000

    created sendemail js for contact us

---
+++ static/js/sendEmail.js
@@ -0,0 +1,22 @@
+const contactUsForm = document.getElementById('contact_form');
+
+
+function sendEmail(contactForm) {
+    emailjs.init("PnNIz_5JC9qfsENAO");
+    emailjs.send("service_ia2pdsh", "Better",{
+        "from_name":contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "request": contactForm.projectsummary.value
+    })
+    .then(
+        function (response) {
+			console.log("SUCCESS", response);
+		},
+		function (error) {
+			console.log('Failed to send', error); 
+			failed();
+		}
+	);
+    return false;
+    }
+
